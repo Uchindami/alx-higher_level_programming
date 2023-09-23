cac1d477448588cc36ada3d735048285b46ed524
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-23 12:30:04 +0200

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char_code in range(97, 123):
     if chr(char_code) not in ('q', 'e'):
-        print(f'{chr(char_code)}', end='')
+        print("{}".format(chr(char_code)), end="")
