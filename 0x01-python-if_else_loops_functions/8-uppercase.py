ccd30c28ac4968560086cc572de01d5e28f90bc1
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-23 12:34:20 +0200

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,8 +6,8 @@ def uppercase(input_str):
     for char in input_str:
         if islower(char):
             uppercase_char = chr(ord(char) - ord('a') + ord('A'))
-            print(uppercase_char, end='')
+            print("{}".format(uppercase_char), end="")
         else:
-            print(char, end='')
+            print("{}".format(char), end="")
     
     print()
