f029b5e841fbf04606e2808981083b9b8e82b4d1
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-24 22:23:53 +0200

    two

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,7 @@
 
 def element_at(my_list, idx, element):
     """ Replace an element from a list like in C """
-    if idx < 0 or idx >= len(my_list):
-        return my_list
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
 
-    my_list[idx] = element
-
-    return my_list
