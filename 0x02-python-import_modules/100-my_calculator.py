6613e0074768f83ace146c3c961806f8f31aa2ff
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-24 20:50:28 +0200

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,17 +2,23 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
 
-options = {"+": add, "-": sub, "*": mul, "/": div}
-operator = sys.argv[2]
+def my_calculator():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-if operator not in options:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    options = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = sys.argv[2]
 
-a, b = int(sys.argv[1]), int(sys.argv[3])
-result = options[operator](a, b)
-print("{} {} {} = {}".format(a, operator, b, result))
+    if operator not in options:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a, b = int(sys.argv[1]), int(sys.argv[3])
+    result = options[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+
+if __name__ == "__main__":
+    my_calculator()
