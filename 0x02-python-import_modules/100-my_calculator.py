369270c8c3cfe5584df317c0143a8c827fd48a75
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-24 20:48:30 +0200

    👨🏿‍💻

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,17 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
-def main():
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+options = {"+": add, "-": sub, "*": mul, "/": div}
+operator = sys.argv[2]
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+if operator not in options:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
-
-
-if __name__ == "__main__":
-    main()
+a, b = int(sys.argv[1]), int(sys.argv[3])
+result = options[operator](a, b)
+print("{} {} {} = {}".format(a, operator, b, result))
