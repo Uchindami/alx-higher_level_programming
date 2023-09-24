3968977288c165a73c5e4105674a3ec49dac9262
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-24 22:29:05 +0200

    x

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
     """ Replace the girl bro, Too many fish """
     if idx < 0 or idx >= len(my_list):
-        return my_list  
+        return my_list
     else:
         my_list[idx] = element
         return my_list
