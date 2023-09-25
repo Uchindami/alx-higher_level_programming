d10e91d69bc9ea64ca6b4fc7571506c677f29e16
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-25 09:55:20 +0200

    101 easy

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-
-def main():
-    __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
-
-
-if __name__ == "__main__":
-    main()
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
